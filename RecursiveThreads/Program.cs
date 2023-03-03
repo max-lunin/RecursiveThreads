755c8d9d40f355efc1b7e549c58c841e6993ff31
Max <max.lunin@gmail.com>
2023-03-03 22:55:24 +0200

    Add project files.

---
+++ RecursiveThreads/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Recursive();
+    }
+
+    static void Recursive()
+    {
+        Thread thread = new Thread(() =>
+        {
+            Console.WriteLine(
+                              $"Thread ID: {Thread.CurrentThread.ManagedThreadId}, " +
+                              $"Total threads: {Process.GetCurrentProcess().Threads.Count}");
+            Recursive();
+        });
+        thread.IsBackground = true;
+        thread.Start();
+        thread.Join();
+    }
+}
